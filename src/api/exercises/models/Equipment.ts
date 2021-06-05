e2e4328f800d008531fd907b7299a9dfd2a807cc
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 18:21:23 -0400

    Add Equipment enum

---
+++ src/api/exercises/models/Equipment.ts
@@ -0,0 +1,13 @@
+enum Equipment {
+  band = "band",
+  foamRoll = "foam roll",
+  barbell = "barbell",
+  kettlebell = "kettlebell",
+  machine = "machine",
+  cable = "cable",
+  medicineBall = "medicine ball",
+  dumbbell = "dumbbell",
+  none = "none"
+}
+
+export default Equipment;
