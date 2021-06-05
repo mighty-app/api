6013d0e1d52714e5ce07b925a004e8dd02b7763a
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 19:58:00 -0400

    Add route that returns spec

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import RealSpecService from "./api/spec/services/SpecService";
+
+const router = express.Router();
+
+router.get("/spec", new RealSpecService().getSpec);
