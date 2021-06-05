547e0b2ed1905cd5cd798384da582a5de604b79c
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 17:34:59 -0400

    Add GraphQL

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
+import { ApolloServer } from "apollo-server-express";
 import "reflect-metadata";
 import app from "./app";
+import buildSchema from "./buildSchema";
 
 const main = async () => {
   const port = process.env.PORT || 5000;
 
-  app.listen({ port }, () => console.log(`🚀 Server ready and listening at ==> http://localhost:${port}`));
+  const schema = await buildSchema();
+
+  const server = new ApolloServer({ schema });
+  server.applyMiddleware({ app });
+
+  app.listen({ port }, () =>
+    console.log(`🚀 Server ready and listening at ==> http://localhost:${port}${server.graphqlPath}`)
+  );
 };
 
 main().catch((error) => console.log("🛑 Error:", error));
