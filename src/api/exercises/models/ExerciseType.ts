c20d3d0ae65ea5d165a1307876df1d42c1c4e488
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 18:21:30 -0400

    Add ExerciseType enum

---
+++ src/api/exercises/models/ExerciseType.ts
@@ -0,0 +1,10 @@
+enum ExerciseType {
+  running = "running",
+  lifting = "lifting",
+  plyometrics = "plyometrics",
+  stretching = "stretching",
+  biking = "biking",
+  swimming = "swimming"
+}
+
+export default ExerciseType;
