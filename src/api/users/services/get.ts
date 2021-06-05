986cb47965b8f1f7a0e559344378e6832dfa07f2
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 19:23:23 -0400

    Organize imports

--- src/api/users/services/get.ts
+++ src/api/users/services/get.ts
@@ -1,5 +1,5 @@
-import User from "../models/User";
 import { UserModel } from "../../../models";
+import User from "../models/User";
 
 export default async function getUser(userId: string): Promise<User | null> {
   return await UserModel.findById(userId);
