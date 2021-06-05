c0dca0644701f3adf6e78ca2b71453996c6e67e1
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-05 17:35:28 -0400

    Refactor UserService

--- src/api/users/controllers/UserController.ts
+++ src/api/users/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Path, Route, Tags } from "tsoa";
 import User from "../models/User";
-import UserService from "../services/UserService";
+import RealUserService from "../services/UserService";
 
 @Route("users")
 @Tags("User")
@@ -8,6 +8,6 @@ export class UserController extends Controller {
   /** Get user by ID */
   @Get("{userId}")
   async getUser(@Path() userId: string): Promise<User | null> {
-    return new UserService().getUser(userId);
+    return new RealUserService().getUser(userId);
   }
 }
--- src/api/users/services/UserService.ts
+++ src/api/users/services/UserService.ts
@@ -1,7 +1,11 @@
 import User from "../models/User";
 import get from "./get";
 
-export default class UserService {
+interface UserService {
+  getUser(userId: string): Promise<User | null>;
+}
+
+export default class RealUserService implements UserService {
   public async getUser(userId: string): Promise<User | null> {
     return await get(userId);
   }
