9ec1b66ac081b19e155787636e6d185774779a4d
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-06 10:22:45 -0400

    Add Muscle model

--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,8 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import Exercise from "./api/exercises/models/Exercise";
+import Muscle from "./api/muscles/models/Muscle";
 import User from "./api/users/models/User";
 
 export const ExerciseModel = getModelForClass(Exercise);
+export const MuscleModel = getModelForClass(Muscle);
 export const UserModel = getModelForClass(User);
