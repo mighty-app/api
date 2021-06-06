e6ad55aff392652a3f2318b45118157f60bedd6f
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-06 10:41:32 -0400

    Fix muscle parameter type

--- src/api/muscles/services/getExercises.ts
+++ src/api/muscles/services/getExercises.ts
@@ -1,9 +1,11 @@
+import { DocumentType } from "@typegoose/typegoose";
 import { MuscleModel } from "../../../models";
 import Exercise from "../../exercises/models/Exercise";
+import Muscle from "../models/Muscle";
 
 export default async function getExercises(muscleId: string): Promise<Exercise[] | null> {
   return await MuscleModel.findById(muscleId)
     .populate("exercises")
     .exec()
-    .then((muscle) => muscle?.exercises ?? null);
+    .then((muscle: DocumentType<Muscle> | null) => muscle?.exercises ?? null);
 }
