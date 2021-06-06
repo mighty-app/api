265156c44b718765a204d417b324da1f66e11743
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-06 16:09:18 -0400

    Fix return

--- src/api/users/services/get.ts
+++ src/api/users/services/get.ts
@@ -2,5 +2,6 @@ import { UserModel } from "../../../models";
 import { SafeUser } from "../models/SafeUser";
 
 export default async function getUser(userId: string): Promise<SafeUser | null> {
-  return await UserModel.findById(userId).toSafeUser();
+  const user = await UserModel.findById(userId);
+  return user?.toSafeUser() ?? null;
 }
