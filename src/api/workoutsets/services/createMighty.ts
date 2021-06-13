ef6f061b6d51579d71d6834c5c85d382491f731c
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-13 17:21:55 -0400

    Update to add Mighty workout set to Mighty workout

--- src/api/workoutsets/services/createMighty.ts
+++ src/api/workoutsets/services/createMighty.ts
@@ -1,6 +1,25 @@
-import { MightyWorkoutSetModel } from "../../../models";
+import { MightyWorkoutNotFound } from "../../../errors";
+import { MightyWorkoutModel, MightyWorkoutSetModel } from "../../../models";
 import MightyWorkoutSet from "../models/MightyWorkoutSet";
 
 export default async function createMighty(reps: number, pounds: number, workoutId: string): Promise<MightyWorkoutSet> {
-  return await MightyWorkoutSetModel.create({ reps, pounds, workout: workoutId, timestamp: new Date() });
+  try {
+    const mightyWorkout = await MightyWorkoutModel.findById(workoutId);
+
+    if (!mightyWorkout) throw new MightyWorkoutNotFound();
+
+    const mightyWorkoutSet = await MightyWorkoutSetModel.create({
+      reps,
+      pounds,
+      workout: workoutId,
+      timestamp: new Date()
+    });
+
+    if (mightyWorkout.workoutSets) mightyWorkout.workoutSets.push(mightyWorkoutSet.id);
+    else mightyWorkout.workoutSets = [mightyWorkoutSet.id];
+
+    return mightyWorkoutSet;
+  } catch (error) {
+    throw error;
+  }
 }
