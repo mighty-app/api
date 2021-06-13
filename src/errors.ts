8b57f16f266b954de1890f50810017cd2c30fd44
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-13 19:40:37 -0400

    Refactor MightyError

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,22 @@
-interface DbError {
+export interface MightyError {
   message: string;
+  error?: any;
 }
 
-export class MightyWorkoutNotFound implements DbError {
-  message = "Mighty workout not found.";
+export class MightyWorkoutNotFound implements MightyError {
+  readonly message = "Mighty workout not found";
+  readonly error?: any;
+
+  constructor(error?: any) {
+    this.error = error;
+  }
+}
+
+export class InvalidUpdateKey implements MightyError {
+  readonly message = "Invalid update key";
+  readonly error?: any;
+
+  constructor(error?: any) {
+    this.error = error;
+  }
 }
