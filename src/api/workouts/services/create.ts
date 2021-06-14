3e71a2e4bfc7226b195d58f519b1727b3174752d
Matt Ramotar <matt.ramotar@jhu.edu>
2021-06-14 09:27:15 -0400

    Add workout to user's authored workouts

--- src/api/workouts/services/create.ts
+++ src/api/workouts/services/create.ts
@@ -1,4 +1,4 @@
-import { WorkoutModel } from "../../../models";
+import { UserModel, WorkoutModel } from "../../../models";
 import Workout from "../models/Workout";
 
 export default async function create(
@@ -7,5 +7,18 @@ export default async function create(
   workoutSets: string[],
   author: string
 ): Promise<Workout> {
-  return await WorkoutModel.create({ name, description, workoutSets, author });
+  try {
+    const workout = await WorkoutModel.create({ name, description, workoutSets, author });
+
+    const user = await UserModel.findById(author);
+
+    if (!user) throw new Error();
+
+    if (user.workoutsAuthored) user.workoutsAuthored.push(workout.id);
+    else user.workoutsAuthored = [workout.id];
+
+    return workout;
+  } catch (error) {
+    throw error;
+  }
 }
